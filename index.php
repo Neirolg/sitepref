9c35a31c1f7ad89ed7391ca9a4a14c036db821a7
Luis Paulo da Silva <49842531+Neirolg@users.noreply.github.com>
2023-07-04 16:18:59 -0300

    primeira atualização

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sala do Empreendedor</title>
+</head>
+
+<body>
+    <nav>
+        <ul class="menu">
+            <li><a href="index.php">Início</a></li>
+            <li><a href="saladigital.php">Sala Digital</a></li>
+            <li><a href="contato.php">Contato</a>
+            <li><a href="sobre.php">Sobre Nós</a></li>
+            <li><a href="agenda2023.php">Agenda 2023</a></li>
+            <li><a href="empregapalhoca.php">Emprega Palhoça</a></li>
+            <li><a href="licitacoes.php">Licitações</a></li>
+            <li><a href="gestaoemfinancas.php">Gestão em Finanças</a></li>
+            <li><a href="notafiscalnacional.php">Nota Fiscal Nacional</a></li>
+        </ul>
+    </nav>
+<h1>Bem-vindo a Sala do Empreendedor de Palhoça</h1>
+<h2>Conheça os serviços da Sala do Empreendedor</h2>
+<h3>Atendimento à todas as empresas e empreendedores do município de Palhoça</h3>
+<a href='https://www.freecodecamp.org/'><button>Link para o freeCodeCamp</button></a>
+
+</body>
+
+</html>
