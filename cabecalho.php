e0ab9a3f497e6e949f8b3b1d5748a1e4c9f62e74
Luis Paulo da Silva <49842531+Neirolg@users.noreply.github.com>
2023-07-13 18:52:43 -0300

    mudanças no cabeçalho

--- cabecalho.php
+++ cabecalho.php
@@ -1,54 +1,157 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <title>Cabeçalho com Carrossel</title>
+    <title>Cabeçalho com Fundo de Imagem</title>
     <style>
-        /* Estilos para o carrossel */
-        .carousel {
-            width: 33%;
-            height: 33%;
-            position: relative;
+        /* Estilos para o cabeçalho */
+        header {
+            width: 100%;
+            max-height: 40vh;
+            background-image: url('palhoca/palhoca3.jpg');
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+
+        /* Estilos para o conteúdo do cabeçalho */
+        .header-content {
+            padding: 40px;
+            color: #fff;
+        }
+
+        /* Estilos para o menu horizontal */
+        ul.menu {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        ul.menu li {
+            float: left;
+        }
+
+        ul.menu li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        ul.menu li a:hover {
+            background-color: #111;
+        }
+
+        /* Estilos para o menu de hambúrguer */
+        .menu-hamburguer {
+            display: none;
+            background-color: #333;
             overflow: hidden;
         }
-        
-        .carousel-inner {
-            width: 100%;
-            height: 100%;
-            display: flex;
-            transition: transform 5s ease;
-            animation: carousel-rotation 30s infinite linear;
+
+        .menu-hamburguer a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
         }
-        
-        .carousel-item {
-            width: 100%;
-            height: 100%;
-            flex-shrink: 0;
+
+        /* Estilos para o botão de hambúrguer */
+        .hamburguer-btn {
+            display: none;
+            position: absolute;
+            top: 0;
+            right: 0;
+            padding: 15px 20px;
+            background-color: #333;
+            color: #fff;
+            cursor: pointer;
         }
-        
-        /* Animação do carrossel */
-        @keyframes carousel-rotation {
-            0% { transform: translateX(0); }
-            33.33% { transform: translateX(-100%); }
-            66.66% { transform: translateX(-200%); }
-            100% { transform: translateX(0); }
+
+        /* Estilos para o menu responsivo */
+        @media screen and (max-width: 600px) {
+            ul.menu li:not(:first-child) {
+                display: none;
+            }
+
+            ul.menu li.hamburguer-icon {
+                display: block;
+                float: right;
+            }
+
+            .menu-hamburguer.responsive {
+                position: relative;
+                display: block;
+            }
+
+            .menu-hamburguer.responsive a {
+                float: none;
+                display: block;
+                text-align: left;
+            }
         }
     </style>
 </head>
+
 <body>
-    <div class="carousel">
-        <div class="carousel-inner">
-            <?php
-            $images = array(
-                "palhoca1.jpg",
-                "palhoca2.jpg",
-                "palhoca3.jpg"
-            );
-            
-            foreach ($images as $image) {
-                echo '<div class="carousel-item"><img src="palhoca/' . $image . '" alt="Imagem de Palhoça, SC"></div>';
-            }
-            ?>
+    <header>
+        <div class="header-content">
+            <h1>Sala do Empreendedor</h1>
+            <p>Texto de descrição ou informações adicionais.</p>
         </div>
-    </div>
+
+        </head>
+
+        <body>
+            <ul class="menu">
+                <li><a href="index.php">Início</a></li>
+                <li><a href="saladigital.php">Sala Digital</a></li>
+                <li><a href="contato.php">Contato</a>
+                <li><a href="sobre.php">Sobre Nós</a></li>
+                <li><a href="agenda2023.php">Agenda 2023</a></li>
+                <li><a href="empregapalhoca.php">Emprega Palhoça</a></li>
+                <li><a href="licitacoes.php">Licitações</a></li>
+                <li><a href="gestaoemfinancas.php">Gestão em Finanças</a></li>
+                <li><a href="notafiscalnacional.php">Nota Fiscal Nacional</a></li>
+                <li class="hamburguer-icon">
+                    <a href="javascript:void(0);" class="hamburguer-btn" onclick="toggleMenu()">☰</a>
+                </li>
+            </ul>
+
+            <div class="menu-hamburguer" id="menuHamburguer">
+                <a href="index.php">Início</a>
+                <a href="saladigital.php">Sala Digital</a>
+                <a href="contato.php">Contato</a>
+                <a href="sobre.php">Sobre Nós</a>
+                <a href="agenda2023.php">Agenda 2023</a>
+                <a href="empregapalhoca.php">Emprega Palhoça</a>
+                <a href="licitacoes.php">Licitações</a>
+                <a href="gestaoemfinancas.php">Gestão em Finanças</a>
+                <a href="notafiscalnacional.php">Nota Fiscal Nacional</a>
+            </div>
+
+            <script>
+                function toggleMenu() {
+                    var menu = document.getElementById("menuHamburguer");
+                    if (menu.className === "menu-hamburguer") {
+                        menu.className += " responsive";
+                    } else {
+                        menu.className = "menu-hamburguer";
+                    }
+                }
+            </script>
+        </body>
+
+</html>
+<ul class="menu">
+
+</ul>
+</header>
+
+<!-- Resto do conteúdo da página -->
 </body>
+
 </html>
